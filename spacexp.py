0ba5a151eb527cb59492a50ace08ec05ccc0132a
YaroslavSnarski <99130560+YaroslavSnarski@users.noreply.github.com>
2024-10-15 17:47:04 +0300

    goldstandard dataset processed

--- spacexp.py
+++ spacexp.py
@@ -4,5 +4,5 @@ from SpacExp.file_manager import FileManager
 #FileManager("/content/my_files", "output.csv").run()
 
 if __name__ == "__main__":
-    manager = FileManager("D:\\Users\\snarski\\AppData\\Local\\Programs\\Python\\Python311\\hw1_prog\\only_useful_files", "output_gold_standard_data.csv")
+    manager = FileManager("D:\\YandexDisk\\gold_standard_dataset_snarski\\Files", "output_gold_standard_data.csv")
     manager.run()
