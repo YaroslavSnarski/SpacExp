9aaa4ac28dbff28487d97ae8a72f623ffa93d854
YaroslavSnarski <99130560+YaroslavSnarski@users.noreply.github.com>
2024-10-15 11:44:34 +0300

    SpacExp

---
+++ spacexp.py
@@ -0,0 +1,8 @@
+
+from SpacExp.file_manager import FileManager
+
+#FileManager("/content/my_files", "output.csv").run()
+
+if __name__ == "__main__":
+    manager = FileManager("D:\\Users\\snarski\\AppData\\Local\\Programs\\Python\\Python311\\hw1_prog\\only_useful_files", "output.csv")
+    manager.run()
